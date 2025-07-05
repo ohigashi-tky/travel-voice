ef29048f62a5d6b43506a124b3e19c87492901c6
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2025-07-05 14:52:15 +0900

    不要なファイルを削除

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TouristSpotSeeder::class,
             GuideSeeder::class,
-            AudioGuideSeeder::class,
         ]);
     }
 }
