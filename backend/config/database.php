bded91ef4b646d06515c6ae823c218e0fdb3ef68
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2025-07-15 00:10:39 +0900

    fix

--- backend/config/database.php
+++ backend/config/database.php
@@ -33,7 +33,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => null,
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
